2cd244bd501314d5ed2f3532eed236e9df2cb661
Gaurav Sharma <fred65804@gmail.com>
2026-09-14 10:13:20 +0000

    Intentional: fail one test condition

--- add.test.js
+++ add.test.js
@@ -3,7 +3,7 @@ const assert = require('node:assert');
 const add = require('./add.js');
 
 test('adds two positive numbers', () => {
-  assert.strictEqual(add(2,3), 5);
+  assert.strictEqual(add(2,3), 6);
 });
 
 test('adds a negative number', () => {
